f0ca34d198bef59bed061700389c46aa79b30a63
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-01-23 14:01:58 +0100

    Create a basic structure to send props (todo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import { ShowTodo } from './components/ShowTodo'
+import { Todo } from './models/Todo'
 
 function App() {
 
+  const [todos, setTodos] = useState<Todo[]>([
+    new Todo(new Date().toLocaleDateString(), "Hämta paket", "2024/02/02", 5, false),
+    new Todo(new Date().toLocaleDateString(), "Städa", "2024/02/05", 3, false)
+  ])
+
   return (
-    <></>
+    <>
+      <h1>TO DO LIST</h1>
+      <ul>
+        {todos.map((todo) => {
+          return < ShowTodo todo={todo} />
+        })}
+      </ul>
+    </>
   )
 }
 
