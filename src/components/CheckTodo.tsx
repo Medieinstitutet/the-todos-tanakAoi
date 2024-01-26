0e8888fe5619ff159d232b84e870cc007a75e504
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-01-26 14:37:04 +0100

    Style checkbox

--- src/components/CheckTodo.tsx
+++ src/components/CheckTodo.tsx
@@ -9,10 +9,21 @@ interface IChangeTodoProps {
 export const CheckTodo = (props: IChangeTodoProps) => {
 
     const handleChange = ( e: ChangeEvent<HTMLInputElement> ) => {
+
+        if( e.target.nextElementSibling !== null ) {
+            if ( e.target.checked === true ) {
+                e.target.nextElementSibling.textContent = "Not Done"
+            } else {
+                e.target.nextElementSibling.textContent = "Done"
+            }
+        }
+
         props.checkTodo(e.target.name)
     };
 
     return(
-        <input type="checkbox" name={props.todo.task} checked={props.todo.isDone} onChange={handleChange} />
+        <label className="item__checked">
+            <input type="checkbox" name={props.todo.task} checked={props.todo.isDone} onChange={handleChange} /><span>Done</span>
+        </label>
     )
 }
