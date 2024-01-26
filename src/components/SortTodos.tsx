5676e35b58a31bd76553f21d7a3d4e539136d09a
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-01-26 11:57:46 +0100

    Add a component to sort todos

---
+++ src/components/SortTodos.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent } from "react"
+import { Todo } from "../models/Todo"
+
+interface ISortTodosProps {
+    // todos: Todo[],
+    sortTodos: (sortedTodos: Todo[]) => void
+}
+
+export const SortTodos = (props: ISortTodosProps) => {
+
+    const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+    
+    let newTodos: Todo[]
+
+    const handleChange = ( e: ChangeEvent<HTMLSelectElement> ) => {
+        
+        if( e.target.value === "dueDate" ) {
+          newTodos = todos.sort((a:Todo, b: Todo) => a.dueDate > b.dueDate ? 1 : -1 )
+        }
+    
+        if( e.target.value === "priority" ) {
+        //   setSortedTodos(sortedTodos.sort((a, b) => a.priority < b.priority ? 1: -1 ))
+          newTodos = todos.sort((a:Todo, b: Todo) => a.dueDate < b.dueDate ? 1 : -1 )
+        }
+
+    }
+    
+    const handleClick = () => {
+        props.sortTodos(newTodos)
+    }
+
+    return(
+        <>
+            <div>
+                <select name="" id="" onChange={handleChange}>
+                    <option value="">--Sort by---</option>
+                    <option value="dueDate">Due date</option>
+                    <option value="priority">Priority</option>
+                </select>
+                <button onClick={handleClick}>Sort</button>
+            </div>
+        </>
+    )
+}
